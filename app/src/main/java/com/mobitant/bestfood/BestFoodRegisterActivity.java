9936bc3327e29352952cff2fb4cee7ad743b280b
MyungsuWoo <master.sy90@!>
2018-11-05 14:40:59 +0900

    create BestFoodRegisterActivity

---
+++ app/src/main/java/com/mobitant/bestfood/BestFoodRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.mobitant.bestfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BestFoodRegisterActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_best_food_register);
+	}
+}
