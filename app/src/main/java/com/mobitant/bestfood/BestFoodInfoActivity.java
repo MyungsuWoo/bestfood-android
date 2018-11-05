a8e0a99911d7bbce0a24f63a9d9ee12c521c856f
MyungsuWoo <master.sy90@!>
2018-11-05 14:53:21 +0900

    define INFO_SEQ

--- app/src/main/java/com/mobitant/bestfood/BestFoodInfoActivity.java
+++ app/src/main/java/com/mobitant/bestfood/BestFoodInfoActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BestFoodInfoActivity extends AppCompatActivity {
+	public static final String INFO_SEQ = "INFO_SEQ";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
